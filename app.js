972ec3dca43aae615d3dbe57b17c4b862148e1a9
beelmin <belmin.ruhotina@gmail.com>
2019-04-12 11:59:10 +0200

    novo

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+
+var app = express();
+
+const port=process.env.PORT || 3000
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+var conversations = [];
+
+app.get('/', function (req, res,next) {
+  res.json({
+    'messages' : conversations
+  }); 
+});
+
+
+app.post('/', function(req, res, next) {
+  var newMessage = {
+  	user : req.body.username,
+  	mess : req.body.message
+  };
+  conversations.push(newMessage);
+  /*
+  res.json({
+  	success: true
+  });
+  */
+});
+
+
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!');
+});
