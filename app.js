aede8f561fa722d8859967f4de5db3104796d2af
belmin <belmin.ruhotina@gmail.com>
2019-04-25 14:06:47 +0200

    novo

--- app.js
+++ app.js
@@ -9,19 +9,33 @@ const port=process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname + '/uploads'));
+
 
 var conversations = [];
+
+var name;
 
 
 
 app.post('/', upload.single('slika'), function (req, res, next) {
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
+
+  name = req.file.path;
   res.send({
   	file: req.file
   });
 });
 
+
+app.get('/',function(req,res,next){
+
+
+	res.sendFile(path.resolve(__dirname, '/',name));
+  	res.end()
+
+});
 
 /*
 
